7e907ebeab0c01e1748e9c3971519fec016ec869
wasiu15 <crownprincewadas>
2021-08-23 09:19:11 +0800

    added material ui

---
+++ src/Components/Order/OrderForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from "../../layouts/Form"
+
+export default function OrderForm() {
+    return (
+        <Form>
+            <input />
+        </Form>
+    )
+}
---
+++ src/Components/Order/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import OrderForm from './OrderForm'
+
+export default function Order() {
+    return (
+        <OrderForm />
+    )
+}
