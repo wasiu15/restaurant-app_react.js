405ca6d86c6b0c38cd3e2d62ac13beb618a81a66
wasiu15 <crownprincewadas>
2021-08-24 02:48:44 +0800

    Interface design

--- src/Components/Order/OrderForm.js
+++ src/Components/Order/OrderForm.js
@@ -1,10 +1,120 @@
-import React from 'react'
-import Form from "../../layouts/Form"
+import React, { useState } from "react";
+import Form from "../../Layouts/Form";
+import {
+  ButtonGroup,
+  Button as MuiButton,
+  Grid,
+  InputAdornment,
+  makeStyles,
+} from "@material-ui/core";
+import { Input, Select, Button } from "../../Controls";
+import ReplayIcon from "@material-ui/icons/Replay";
+import RestaurantMenuIcon from "@material-ui/icons/RestaurantMenu";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
-export default function OrderForm() {
-    return (
-        <Form>
-            <input />
-        </Form>
-    )
+const pMethods = [
+  { id: "none", title: "Select" },
+  { id: "Cash", title: "Cash" },
+  { id: "Card", title: "Card" },
+];
+
+const useStyles = makeStyles((theme) => ({
+  adornmentText: {
+    "& .MuiTypography-root": {
+      color: "#f3b33d",
+      fontWeight: "bolder",
+      fontSize: "1.5em",
+    },
+  },
+  submitButtonGroup: {
+    backgroundColor: "#f3b33d",
+    color: "#000",
+    margin: theme.spacing(1),
+    "& .MuiButton-label": {
+      textTransform: "none",
+    },
+    "&:hover": {
+      backgroundColor: "#f3b33d",
+    },
+  },
+}));
+
+export default function OrderForm(props) {
+  const { values, errors, handleInputChange } = props;
+  const classes = useStyles();
+  return (
+    <Form>
+      <Grid container>
+        <Grid item xs={6}>
+          <Input
+            disabled
+            label="Order Number"
+            name="orderNumber"
+            value={values.orderNumber}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment
+                  className={classes.adornmentText}
+                  position="start"
+                >
+                  #
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Select
+            label="Customer"
+            name="customerId"
+            onChange={handleInputChange}
+            value={values.customerId}
+            options={[
+              { id: 0, title: "Select" },
+              { id: 1, title: "Customer 1" },
+              { id: 2, title: "Customer 2" },
+              { id: 3, title: "Customer 3" },
+              { id: 4, title: "Customer 4" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Select
+            label="Payment Method"
+            name="pMethod"
+            onChange={handleInputChange}
+            value={values.pMethod}
+            options={pMethods}
+          />
+          <Input
+            disabled
+            label="Grand Total"
+            name="gTotal"
+            value={values.gTotal}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment
+                  className={classes.adornmentText}
+                  position="start"
+                >
+                  $
+                </InputAdornment>
+              ),
+            }}
+          />
+          <ButtonGroup className={classes.submitButtonGroup}>
+            <MuiButton
+              size="large"
+              endIcon={<RestaurantMenuIcon />}
+              type="submit"
+            >
+              Submit
+            </MuiButton>
+            <MuiButton size="small" startIcon={<ReplayIcon />} />
+          </ButtonGroup>
+          <Button size="large" startIcon={<ReorderIcon />}>
+            Order
+          </Button>
+        </Grid>
+      </Grid>
+    </Form>
+  );
 }
--- src/Components/Order/index.js
+++ src/Components/Order/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import OrderForm from './OrderForm'
+import React from "react";
+import OrderForm from "./OrderForm";
+import { useForm } from "../../hooks/useForm";
 
 export default function Order() {
-    return (
-        <OrderForm />
-    )
+  const generateOrderNumber = () =>
+    Math.floor(100000 + Math.random() * 900000).toString();
+
+  const getFreshModelObject = () => ({
+    orderMasterId: 0,
+    orderNumber: generateOrderNumber(),
+    customerId: 0,
+    pMethod: "none",
+    gTotal: 0,
+    deletedOrderItemIds: "",
+    orderDetails: [],
+  });
+
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetFormControls,
+  } = useForm(getFreshModelObject);
+
+  return <OrderForm {...{ values, errors, handleInputChange }} />;
 }
